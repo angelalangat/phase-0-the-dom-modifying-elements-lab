9a045669366fecab6152c681a35eee6fa76d9c38
angie <angelaclangat@gmai.com>
2024-06-22 22:16:08 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// Select the <main> element with the id 'main'
+let mainElement = document.getElementById('main');
+
+// Check if the element exists before trying to remove it
+if (mainElement) {
+    mainElement.remove();
+}
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Angela is the champion';
+
